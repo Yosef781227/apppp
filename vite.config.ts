c9604243e6dcdbfa53d5fb97e9b3d50a27f2cb5c
yosef781227 <joc781227@gmail.com>
2025-03-26 15:03:03 +0000

    final

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
-import { resolve } from 'path';
 
 export default defineConfig({
    base: "/",
    build: {
       outDir: 'dist',
+      rollupOptions: {
+         input: {
+            main: 'index.html',
+            
+         }
+      }
    },
-   resolve: {
-      alias: {
-         '@': resolve(__dirname, './src'),
-      },
-   },
+   server: {
+      port: 3000,
+   
+   }
 });
